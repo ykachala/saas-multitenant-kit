1924b42bba1c80d39814a864ea888d20bf74b106
Yoweli Kachala <yowelikachala@gmail.com>
2025-01-04 18:33:38 +0200

    Add TenantScope global Eloquent scope

---
+++ app/Scopes/TenantScope.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class TenantScope implements Scope
+{
+    public function apply(Builder $builder, Model $model): void
+    {
+        if (($tenant = tenant()) !== null) {
+            $builder->where($model->getTable() . '.tenant_id', $tenant->id);
+        }
+    }
+}
